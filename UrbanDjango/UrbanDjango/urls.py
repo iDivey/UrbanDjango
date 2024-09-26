fc6994061953c9e9d8495d10c1b57a6100c7d1bc
iDivey <150763080+iDivey@users.noreply.github.com>
2024-09-26 16:14:32 +0700

    Функциональное и классовое представление

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from UrbanDjango.task2.views import func
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('func/', func),
+    path('class/', TemplateView.as_view('class_template.html'))
+
 ]
